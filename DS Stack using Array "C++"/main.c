665b17ad1d6b201246d540c44ba9fcf47d7f83c4
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-24 07:39:30 +0200

    Create main.c

---
+++ DS Stack using Array "C++"/main.c
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "stack.h"
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+
+    return 0;
+}
