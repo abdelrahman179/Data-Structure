1784b3a386445f744b205e51fb60c3fb7fec9b64
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-24 07:40:32 +0200

    Create stack.h

---
+++ DS Stack using Array "C++"/stack.h
@@ -0,0 +1,24 @@
+
+#define MAX 1000
+
+class Stack
+{
+    int top;
+
+public:
+    int a[MAX]; // Maximum size of Stack
+
+    Stack() { top = -1; }
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool push(int x);
+
+int pop();
+
+int peek();
+
+bool isempty();
